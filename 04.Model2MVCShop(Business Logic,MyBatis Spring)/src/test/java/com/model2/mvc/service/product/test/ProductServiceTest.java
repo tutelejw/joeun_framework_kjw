c5e22f3c300815bf398426530dc64883325a8651
home_3600 <home_3600@DESKTOP-PARCR2F>
2025-09-12 01:26:26 +0900

    fd

--- 04.Model2MVCShop(Business Logic,MyBatis Spring)/src/test/java/com/model2/mvc/service/product/test/ProductServiceTest.java
+++ 04.Model2MVCShop(Business Logic,MyBatis Spring)/src/test/java/com/model2/mvc/service/product/test/ProductServiceTest.java
@@ -32,6 +32,7 @@ public class ProductServiceTest {
     @Before
     public void setUp() {
         testProduct = new Product();
+        testProduct.setProdNo(10042);
         testProduct.setProdName("테스트상품");
         testProduct.setProdDetail("테스트 상세정보");
         testProduct.setManuDay("20250101");
@@ -44,11 +45,11 @@ public class ProductServiceTest {
         System.out.println("==> 테스트 종료");
     }
 
-    @Test
+    //@Test
     public void testAddAndGetProduct() throws Exception {
         productService.addProduct(testProduct);
-        int prodNo = 10042; // addProduct 후 prodNo가 세팅되어 있어야 함
-//        int prodNo = testProduct.getProdNo(); // addProduct 후 prodNo가 세팅되어 있어야 함
+        //int prodNo = 10042; // addProduct 후 prodNo가 세팅되어 있어야 함
+        int prodNo = testProduct.getProdNo(); // addProduct 후 prodNo가 세팅되어 있어야 함
 
         Product result = productService.getProduct(prodNo);
 
@@ -58,11 +59,11 @@ public class ProductServiceTest {
         System.out.println("==> testAddAndGetProduct 완료");
     }
 
-    //@Test
+    @Test
     public void testUpdateProduct() throws Exception {
         productService.addProduct(testProduct);
         int prodNo = testProduct.getProdNo();
-
+        System.out.println("testUpdateProduct : prodNo : " + prodNo);
         Product updated = productService.getProduct(prodNo);
         updated.setPrice(20000);
         updated.setProdDetail("수정된 상세정보");
@@ -72,10 +73,11 @@ public class ProductServiceTest {
         Product result = productService.getProduct(prodNo);
         assertEquals(20000, result.getPrice());
         assertEquals("수정된 상세정보", result.getProdDetail());
+        System.out.println(result);
         System.out.println("==> testUpdateProduct 완료");
     }
 
-   // @Test
+    @Test
     public void testGetProductList() throws Exception {
         Search search = new Search();
         search.setCurrentPage(1);
@@ -86,7 +88,7 @@ public class ProductServiceTest {
         Map<String, Object> map = productService.getProductList(search);
         assertNotNull(map.get("list"));
         assertTrue(((java.util.List<?>) map.get("list")).size() >= 0);
-
+        System.out.println("map get : " + map.get("list"));
         System.out.println("==> testGetProductList 결과 : " + map.get("list"));
     }
 
